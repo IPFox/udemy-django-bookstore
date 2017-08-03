2abd6e20b1654d90a740e04b4a789b98c3ff1818
Juska Kettunen <ipfox.contact@gmail.com>
2017-08-03 22:23:25 +0300

    added static file compression + template fixes

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'compressor',
     'social_django',
     'registration',
     'bootstrap3',
@@ -142,8 +143,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+COMPRESS_ENABLED = True
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 # User registration options
 ACCOUNT_ACTIVATION_DAYS = 7
 REGISTRATION_AUT_LOGIN = True
